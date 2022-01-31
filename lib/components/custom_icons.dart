af1597eb5db3f99559ab6d0aec7ac1f48427c8bf
Manan Singh <manan@baetobe.com>
2022-01-31 16:48:18 +0530

    adds bottom navigation bar

--- lib/components/custom_icons.dart
+++ lib/components/custom_icons.dart
@@ -21,9 +21,14 @@ class BTBCustomIcons {
   static const _kFontFam = 'BTBCustomIcons';
   static const String? _kFontPkg = null;
 
-  static const IconData btbheart = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData home = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData profile = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData chat = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData likes = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData btbheart =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData profile =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData chat =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData likes =
+      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
