a75b5a6f199e68b1eb3375e11bd116bc2124623f
Manan Singh <manan@baetobe.com>
2022-02-03 19:03:49 +0530

    [WIP] removes unused imports

--- lib/screens/profile_screens/edit_profile_page.dart
+++ lib/screens/profile_screens/edit_profile_page.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:baetobe/application/routing/router_provider.dart';
-import 'package:baetobe/components/custom_divider.dart';
 import 'package:baetobe/components/custom_header_tile.dart';
 import 'package:baetobe/components/edit_profile_tile.dart';
 import 'package:baetobe/components/forms/big_text_field.dart';
@@ -12,7 +11,6 @@ import 'package:baetobe/domain/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:getwidget/components/list_tile/gf_list_tile.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:styled_widget/styled_widget.dart';
 
