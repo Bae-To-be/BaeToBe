0c2b433ea7481d60da1c09b2cd2779e13074bb46
Manan Singh <manan@baetobe.com>
2022-02-04 12:40:24 +0530

    refactors religion state provider

--- lib/domain/form_states/religion_state_provider.dart
+++ lib/domain/form_states/religion_state_provider.dart
@@ -1,21 +1,8 @@
-import 'package:baetobe/domain/background_fields/religion_provider.dart';
 import 'package:baetobe/domain/user_provider.dart';
-import 'package:collection/collection.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final religionStateProvider = StateProvider.autoDispose<int?>((ref) {
-  int? result;
   final user = ref.watch(userProvider);
-  final religions = ref.watch(religionProvider);
-  if (user.religion != null) {
-    religions.whenData((listing) {
-      final match = listing
-          .firstWhereOrNull((religion) => religion.name == user.religion?.name);
-      if (match != null) {
-        result = match.id;
-      }
-    });
-  }
 
-  return result;
+  return user.religion?.id;
 });
