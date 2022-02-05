71997e9f1a3afe2e04dbbd96bec73779094c91d3
gauravrakheja <gauravrakheja00@gmail.com>
2022-02-05 18:12:32 +0530

    CORE-27 Replaces identity state class with generated one

--- lib/domain/form_states/identity_verification_state_provider.dart
+++ lib/domain/form_states/identity_verification_state_provider.dart
@@ -5,6 +5,7 @@ import 'package:baetobe/domain/error_provider.dart';
 import 'package:baetobe/domain/loading_provider.dart';
 import 'package:baetobe/domain/verification_files_provider.dart';
 import 'package:baetobe/entities/data/user_verification_file.dart';
+import 'package:baetobe/entities/view_models/identity_state.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -21,15 +22,15 @@ class IdentityFormStateNotifier extends StateNotifier<IdentityState> {
   IdentityFormStateNotifier(this.ref) : super(IdentityState()) {
     final initialIdentity = ref.read(verificationFilesProvider).getIdentity();
     if (initialIdentity != null) {
-      state = state.copyWith(newUploaded: true);
+      state = state.copyWith(uploaded: true);
     }
     ref.listen<UserVerificationFile?>(
         verificationFilesProvider.select((files) => files.getIdentity()),
         (_, identityFile) {
       if (identityFile != null) {
-        state = state.copyWith(newUploaded: true);
+        state = state.copyWith(uploaded: true);
       } else {
-        state = state.copyWith(newUploaded: false);
+        state = state.copyWith(uploaded: false);
       }
     });
   }
@@ -38,7 +39,7 @@ class IdentityFormStateNotifier extends StateNotifier<IdentityState> {
     final loading = ref.read(loadingProvider.notifier);
     final error = ref.read(errorProvider.notifier);
     File? file;
-    state = state.copyWith(newUploading: true);
+    state = state.copyWith(uploading: true);
     loading.state = true;
     try {
       FilePickerResult? result = await FilePicker.platform.pickFiles(
@@ -51,14 +52,14 @@ class IdentityFormStateNotifier extends StateNotifier<IdentityState> {
             .addFile(VerificationTypes.identity, file.path, null);
         loading.state = false;
         state = state.copyWith(
-            newUploading: false, newFileName: result.files.single.name);
+            uploading: false, fileName: result.files.single.name);
       } else {
-        state = state.copyWith(newUploading: false);
+        state = state.copyWith(uploading: false);
         loading.state = false;
         return;
       }
     } catch (e, stacktrace) {
-      state = state.copyWith(newUploading: false);
+      state = state.copyWith(uploading: false);
       loading.state = false;
       await FirebaseCrashlytics.instance.recordError(e, stacktrace);
       error.updateError(ErrorMessages.couldNotPickFile);
@@ -69,38 +70,13 @@ class IdentityFormStateNotifier extends StateNotifier<IdentityState> {
   Future<void> clearFile() async {
     if (state.uploaded) {
       final loading = ref.read(loadingProvider.notifier);
-      state = state.copyWith(newUploading: true);
+      state = state.copyWith(uploading: true);
       loading.state = true;
       await ref
           .read(verificationFilesProvider.notifier)
           .removeFile(VerificationTypes.identity);
       loading.state = false;
-      state = state.copyWith(newUploading: false);
+      state = state.copyWith(uploading: false);
     }
-  }
-}
-
-class IdentityState {
-  bool uploaded;
-  bool uploading;
-  String? fileName;
-
-  IdentityState({this.uploading = false, this.uploaded = false, this.fileName});
-
-  IdentityState copyWith(
-      {bool? newUploading, bool? newUploaded, String? newFileName}) {
-    return IdentityState(
-        uploading: newUploading ?? uploading,
-        fileName: newFileName ?? fileName,
-        uploaded: newUploaded ?? uploaded);
-  }
-
-  @override
-  String toString() {
-    return {
-      'uploading': uploading,
-      'uploaded': uploaded,
-      'fileName': fileName,
-    }.toString();
   }
 }
