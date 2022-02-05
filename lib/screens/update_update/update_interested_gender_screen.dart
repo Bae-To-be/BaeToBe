1c0119154a6871e3e6cca701837713028ab119a3
gauravrakheja <gauravrakheja00@gmail.com>
2022-02-05 10:18:51 +0530

    CORE-27 Installed freezed and replaces gender class with generated code

--- lib/screens/update_update/update_interested_gender_screen.dart
+++ lib/screens/update_update/update_interested_gender_screen.dart
@@ -10,7 +10,7 @@ import 'package:baetobe/constants/typography.dart';
 import 'package:baetobe/domain/background_fields/gender_provider.dart';
 import 'package:baetobe/domain/form_states/interested_gender_state_provider.dart';
 import 'package:baetobe/domain/user_provider.dart';
-import 'package:baetobe/entities/gender.dart';
+import 'package:baetobe/entities/generated/gender.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
