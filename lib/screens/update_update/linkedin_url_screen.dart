a3ffe44ad64d14e41ed16f33064a1b78bb478012
Manan Singh <manan@baetobe.com>
2022-02-09 15:08:00 +0530

    loads user's images and verification files from backend when in registration flow

--- lib/screens/update_update/linkedin_url_screen.dart
+++ lib/screens/update_update/linkedin_url_screen.dart
@@ -7,6 +7,7 @@ import 'package:baetobe/constants/app_constants.dart';
 import 'package:baetobe/constants/app_links.dart';
 import 'package:baetobe/constants/typography.dart';
 import 'package:baetobe/domain/form_states/linkedin_info_state_provider.dart';
+import 'package:baetobe/domain/images_provider.dart';
 import 'package:baetobe/domain/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -36,6 +37,9 @@ class LinkedinUrlScreen extends HookConsumerWidget {
       if (urlState != null && urlController.text != urlState) {
         urlController.text = urlState;
       }
+      WidgetsBinding.instance?.addPostFrameCallback((_) {
+        ref.read(imagesProvider.notifier).loadImages();
+      });
       return null;
     }, [urlState]);
     return FormLayout(
--- lib/screens/update_update/selfie_verification_screen.dart
+++ lib/screens/update_update/selfie_verification_screen.dart
@@ -5,7 +5,10 @@ import 'package:baetobe/components/text_widgets.dart';
 import 'package:baetobe/components/verification/selfie_container.dart';
 import 'package:baetobe/constants/typography.dart';
 import 'package:baetobe/domain/form_states/selfie_verification_state_provider.dart';
+import 'package:baetobe/domain/images_provider.dart';
+import 'package:baetobe/domain/verification_files_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:styled_widget/styled_widget.dart';
@@ -20,6 +23,15 @@ class SelfieVerificationScreen extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(selfieStateProvider);
+
+    useEffect(() {
+      WidgetsBinding.instance?.addPostFrameCallback((_) {
+        if (redirectBack != true) {
+          ref.read(verificationFilesProvider.notifier).loadFiles();
+        }
+      });
+      return null;
+    }, []);
 
     return FormLayout(
       children: [
