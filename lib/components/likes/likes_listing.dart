a2eeba31c252cde920cb97ec6135b284b8b01cfc
Manan Singh <manan@baetobe.com>
2022-02-10 21:12:37 +0530

    [WIP] removes dead code

--- lib/components/likes/likes_listing.dart
+++ lib/components/likes/likes_listing.dart
@@ -1,10 +1,6 @@
 import 'package:baetobe/application/routing/router_provider.dart';
-import 'package:baetobe/application/theme.dart';
-import 'package:baetobe/components/custom_divider.dart';
 import 'package:baetobe/components/refresh_footer.dart';
 import 'package:baetobe/components/refresh_header.dart';
-import 'package:baetobe/components/text_widgets.dart';
-import 'package:baetobe/components/user_avatar.dart';
 import 'package:baetobe/constants/app_constants.dart';
 import 'package:baetobe/constants/app_links.dart';
 import 'package:baetobe/constants/typography.dart';
@@ -12,7 +8,6 @@ import 'package:baetobe/domain/likes_provider.dart';
 import 'package:baetobe/entities/data/like.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:getwidget/getwidget.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:styled_widget/styled_widget.dart';
@@ -127,17 +122,6 @@ class LikesListing extends HookConsumerWidget {
                               ],
                             ),
                           ),
-                          // CustomTextWidget(
-                          //   type: textWidgetType.subtitle1,
-                          //   text:
-                          //       '${likesListing[index].userName.split(' ').first}, 24',
-                          //   withRow: false,
-                          // ).padding(vertical: 4),
-                          // const CustomTextWidget(
-                          //   type: textWidgetType.subtitle1,
-                          //   text: 'WorkTitle, Industry',
-                          //   withRow: false,
-                          // ).padding(vertical: 4),
                         ],
                       );
                     },
@@ -146,30 +130,6 @@ class LikesListing extends HookConsumerWidget {
               ),
             ),
           ),
-          // child: ListView.separated(
-          //   separatorBuilder: (BuildContext context, int index) =>
-          //       const CustomDivider(),
-          //   itemBuilder: (c, i) => GFListTile(
-          //       margin: const EdgeInsets.symmetric(horizontal: 5),
-          //       avatar: UserAvatar(image: likesListing[i].profilePicture),
-          //       color: offWhite,
-          //       subTitle: Text(likesListing[i].summary,
-          //           style: Theme.of(context).textTheme.caption),
-          //       title: Text(likesListing[i].userName,
-          //           style: Theme.of(context)
-          //               .textTheme
-          //               .subtitle1
-          //               ?.copyWith(fontWeight: FontWeight.w500)),
-          //       onTap: () {
-          //         ref.read(routerProvider.notifier).pushNamed(
-          //             AppLinks.profileDetails(likesListing[i].userId));
-          //       },
-          //       icon: CustomTextWidget(
-          //           type: textWidgetType.caption,
-          //           withRow: false,
-          //           text: likesListing[i].timeSinceCreation)),
-          //   itemCount: likesListing.length,
-          // ),
         );
       },
       error: (_error, _) =>
